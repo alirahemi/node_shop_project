09b7db7fb6e3365194589a1cf461bda8cad8db8c
Ali Rahemi <alirahemi@ymail.com>
2019-08-19 20:22:45 +0200

    complete  Template Engine EJS season 6

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const userRoutes = require('./routes/user');
+const adminData = require('./routes/admin');
+const userRoutes = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'view');
 
 //intial parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin',adminRoutes);
+
+app.use('/admin',adminData.routes);
 app.use(userRoutes);
 
 app.use((req, res, next)=>{
-  res.status(404).sendFile(path.join(rootDir,'view','404.html'))
+  res.status(404).render('404', {pageTitle: 'Page not found',
+  path:''});
 });
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const rootDir = require('../util/path');
+const path = require("path");
+const rootDir = require("../util/path");
+const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir,'view','add-product.html'));
+  res.render("add-product", {
+    pageTitle: "Add-Product",
+    path: "/admin/add-product",
+    // formsCSS: true,
+    // productCSS: true,
+    // activeADDProduct: true
+  });
 });
 
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body.title);
+  if (req.body.title !== "") products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
