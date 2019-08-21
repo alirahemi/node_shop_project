81091fa64930dcfd7b39a2a410a1037526661337
Ali Rahemi <alirahemi@ymail.com>
2019-08-21 17:15:04 +0200

    finish exercise with nodejs

--- controllers/products.js
+++ controllers/products.js
@@ -1,30 +1,34 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getADDProduct = (req, res, next) => {
-    res.render("add-product", {
-      pageTitle: "Add-Product",
-      path: "/add-product",
-      // formsCSS: true,
-      // productCSS: true,
-      // activeADDProduct: true
-    });
-  };
+  res.render("admin/add-product", {
+    pageTitle: "Add-Product",
+    path: "/add-product"
+    // formsCSS: true,
+    // productCSS: true,
+    // activeADDProduct: true
+  });
+};
 
-  exports.postADDProduct = (req, res, next) => {
-    if (req.body.title !== "") products.push({ title: req.body.title });
+exports.postADDProduct = (req, res, next) => {
+  if (req.body.title !== "") {
+    const products = new Product(req.body.title);
+    products.save();
     res.redirect("/");
-  };
+  } else res.redirect("/");
+};
 
-  exports.getProduct = (req, res, next) => {
-    // console.log(adminData.products);
-    // console.log(adminData.products.length);
-    res.render("shop", {
+exports.getProduct = (req, res, next) => {
+  // console.log(adminData.products);
+  // console.log(adminData.products.length);
+  Product.fetchAll((products)=>{
+    res.render("shop/product-list", {
       prods: products,
-      pageTitle: "Shop",
-      path: "/",
+      pageTitle: "Product List",
+      path: "/"
       // hasProducts: products.length > 0,
       // activeShop: true,
       // productCSS: true
     });
-  };
-
+  });
+};
